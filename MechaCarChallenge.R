d91480761536b65bb37dd2c62f1d41335b1392aa
olibabba <oliver.dykstra@gmail.com>
2022-07-09 23:49:41 -0500

    add screenshots

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,26 @@ model <- lm(mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehi
 model
 #generate summary statistics
 summary(model) 
+
+# Read in Suspension csv
+suspension <- data.frame(read.csv(file="Suspension_Coil.csv"))
+
+head(suspension)
+
+# creat summart dataframe
+total_summary  <-  summarize(suspension, mean=mean(suspension$PSI), median=median(suspension$PSI), variance=var(suspension$PSI), SD=sd(suspension$PSI))
+total_summary
+
+# create lot summary dataframe
+by_susp <- suspension %>% group_by(Manufacturing_Lot)
+psi_stats <- by_susp %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI), SD=sd(PSI))
+lot_summary <- data.frame(psi_stats)
+lot_summary
+
+# Perform t-test on all lots
+t.test(suspension$PSI, mu=1500)
+
+# perform t-tests on individual lots
+t.test(subset(suspension$PSI, suspension$Manufacturing_Lot =='Lot1'), mu=1500)
+t.test(subset(suspension$PSI, suspension$Manufacturing_Lot =='Lot2'), mu=1500)
+t.test(subset(suspension$PSI, suspension$Manufacturing_Lot =='Lot3'), mu=1500)
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MechaCar_Statistical_Analysis
 MechaCar, is suffering from production troubles that are blocking the manufacturing team’s progress. AutosRUs’ upper management has called on the data analytics team to review the production data for insights that may help the manufacturing team.
+
+
+## Linear Regression to Predict MPG
+
+
+
+## Summary Statistics on Suspension Coils
+
+
+
+## T-Tests on Suspension Coils
+
+
+
+## Study Design: MechaCar vs Competition
